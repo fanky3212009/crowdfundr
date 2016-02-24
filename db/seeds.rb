3198e773439497581d1eb91a8162e14c363faf2a
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-02-24 15:36:36 -0500

    Add Faker gem to project, makes 10 dummy projects but no rewards.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |proj|
+
+  Project.create({
+    title: Faker::App.name,
+     description: Faker::Company.bs,
+     goal: Faker::Number.number(5),
+     end_date: Faker::Date.forward(100)
+     })
+
+end
+
+# 10.times do |reward|
+#
+#   counter = 0
+#     Reward.create({
+#       title: "Gold",
+#       description: Faker::Lorem.sentence,
+#       amount: Faker::Number.number(2),
+# })
+#
+#   end
+#
+#
