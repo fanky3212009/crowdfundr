7942b54eadd739c759fff97de6b18a59803bc4ea
Franky Chau <houun.chau@gmail.com>
2016-02-24 15:41:12 -0500

    Add days_left method

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,13 @@
 class Project < ActiveRecord::Base
   has_many :rewards
   has_many :pledges
-  belongs_to :user 
+  belongs_to :user
 
   accepts_nested_attributes_for :rewards, reject_if: :all_blank, allow_destroy: true
+
+  def days_left()
+    #code
+    time_left = self.end_date - Time.now
+    (time_left / 1.day).ceil
+  end
 end
