3f312070d0b3f9d8bb5be6b93a3bec23bc2457a1
Franky Chau <houun.chau@gmail.com>
2016-02-24 16:16:45 -0500

    Add message to show whether the current user has backed the project or not

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,4 +10,8 @@ class Project < ActiveRecord::Base
     time_left = self.end_date - Time.now
     (time_left / 1.day).ceil
   end
+
+  def backed(current_user)
+    self.pledges.where("user_id = ?", current_user.id) != []
+  end
 end
