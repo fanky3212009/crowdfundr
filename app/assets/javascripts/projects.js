445ee8c45d73348f9d71aefcd753a108238a7136
Qirui Sun <qiruisun@gmail.com>
2016-02-25 17:51:23 -0500

    add page:load to document ready

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('ready', function () {
+$(document).on('ready page:load', function () {
     $('.rewards-form').submit(function (e) {
       e.preventDefault();
 
